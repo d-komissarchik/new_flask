e0321f3cf7accc2c31184b4b5a17db1dc8e057d3
d-komissarchik <0667669609@ukr.net>
2023-10-12 17:51:01 +0300

    added routes

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return '<h1>Главная страница</h1>'\
+
+@app.route('/about')
+def about():
+    return '<h1>Инфо</h1>'
 
 
 if __name__ == '__main__':
