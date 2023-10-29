c0260cdfd4bc41f4311a18aa7e22bc93e10e67f0
komisar <komisar@gmail.com>
2023-10-29 16:20:08 +0300

    SQLAlchemy

--- app.py
+++ app.py
@@ -4,13 +4,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///newflask.db'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# app = Flask(__name__)
+# app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:////home/komisar/PycharmProjects/new_flask/newflask.db'
+# #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
+
+# db = SQLAlchemy() # db intitialized here
+# app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://test.db"
+# db.init_app(app)
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True);
     title = db.Column(db.String(300), nullable=False);
+    text = db.Column(db.Text, nullable=False);
 
 
 @app.route('/')
